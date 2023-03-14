f9e6e436c5fe72a2f4a9ffaa5c125ab4d0a9b0c1
Aditya Khattri <adityakhattri123@gmail.com>
2023-03-15 03:47:47 +0530

    Added get route

--- server.js
+++ server.js
@@ -14,6 +14,23 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
 
+
+app.get("/read/all" ,async (req,res)=>{
+    try{
+        const usersRef = db.collection('users');
+        const response = await usersRef.get();
+        let responseArr= [];
+        response.forEach(doc=>{
+            responseArr.push(doc.data());
+        });
+
+        res.status(200).json({data:responseArr});
+    }
+    catch(err){
+        res.status(500).json({error:err});
+    }
+});
+
 app.post('/create' ,async (req,res)=>{
     try{
         const id = req.body.email;
@@ -23,8 +40,8 @@ app.post('/create' ,async (req,res)=>{
             lastName:req.body.lastName
         };
         // const response = db.collection('users').doc(id).set(userJson);
-        const response = db.collection('users').add(userJson);
-        res.status(200).json({message:"Sucessful"});
+        const response =await db.collection('users').add(userJson);
+        res.status(200).json({message:response});
     }
     catch(err){
         res.status(500).json({error_message:err});
